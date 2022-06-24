566b7cab12e9845e0680b66c6591a839e873990e
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-24 15:44:55 +0530

    home page default content removed

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -1,14 +1,25 @@
+import Image from "next/image";
 import styles from "../../styles/Header.module.scss";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="container">
+      <div className="container groupTwo">
         <div className={styles.logo}>
-          {" "}
           <span>CPM</span> Calculators
         </div>
-        <nav></nav>
+        <nav>
+          <div className="nav-icon">
+            <Image
+              src="https://res.cloudinary.com/dqove2qhg/image/upload/v1656065276/CPM%20Calculators/nav_icon_pl11bj.svg"
+              alt="Vercel Logo"
+              width={72}
+              height={16}
+            />
+
+            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+          </div>
+        </nav>
       </div>
     </header>
   );
