50b70c4a5aa995af95392e280de63ef676213005
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-24 17:24:24 +0530

    addition of about cpm styles

--- components/AboutCPM/index.jsx
+++ components/AboutCPM/index.jsx
@@ -29,6 +29,23 @@ const AboutCPM = () => {
             />
           </div>
         </div>
+
+        <div className={styles.whatIsCPM}>
+          <h3>What is CPM?</h3>
+
+          <div className={styles.text}>
+            CPM is one of the most significant advertising metrics. CPM means
+            cost per mille, i.e. the cost per thousand impressions. Simply it
+            means the cost of having an ad published, seen a thousand times on a
+            particular website, and is used to inspect and oversee ad
+            performance. Since early online marketing campaigns, CPM has acted
+            as an industry standard that determines web ads’ advertising costs
+            and pricing. CPM calculation is simple – take the total cost paid in
+            CPM campaigns and then multiply the total impressions by the CPM
+            rate and then divide it by one thousand. For example, one million
+            impressions at 1 $ CPM equals $1000 in gross revenue.
+          </div>
+        </div>
       </div>
     </section>
   );
