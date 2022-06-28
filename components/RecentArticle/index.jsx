bbbac3ffc6723553263eb5980181b25a405e9613
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-28 15:25:02 +0530

    recent articles mobile styling done

--- components/RecentArticle/index.jsx
+++ components/RecentArticle/index.jsx
@@ -1,10 +1,50 @@
+import Image from "next/image";
+
 import styles from "../../styles/RecentArticle.module.scss";
+
+const data = [
+  {
+    id: "What is Ad Fill Rate?)",
+    title: "What is Ad Fill Rate?",
+    src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656409762/CPM%20Calculators/Ad-Fill-Rate_fpmgvq.jpg",
+    para: "Introduction Modern-day websites are attractive digital constructs that relay pertinent information to the visitors while maximizing their user experience. The primary objective …",
+  },
+  {
+    id: "What is CPV?",
+    title: "What is CPV?",
+    src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656409745/CPM%20Calculators/What-is-CPV_klbbkh.jpg",
+    para: "Introduction Online advertising is a dynamic part of the digital world. Advertising comes in many shapes and forms yet always has to …",
+  },
+];
 
 const RecentArticle = () => {
   return (
     <section className={styles.sectionContainer}>
       <div className="container">
         <h2>Recent Articles</h2>
+
+        <div className={styles.articles}>
+          {data.map((info) => {
+            return (
+              <div key={info.id} className={styles.article}>
+                <div>
+                  <Image
+                    src={info.src}
+                    alt=""
+                    layout="responsive"
+                    width={100}
+                    height={50}
+                  />
+                </div>
+                <div className={styles.text}>
+                  <h3>{info.title}</h3>
+
+                  <p>{info.para}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
