16ebe37d80638594ee333b61e2cf38e759273855
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-28 20:06:37 +0530

    hero section big screen style addition

--- components/Hero/Home/index.jsx
+++ components/Hero/Home/index.jsx
@@ -1,20 +1,32 @@
-import styles from "../../../styles/HeroHome.module.scss";
+import Image from "next/image";
+
 import CalculatorForm from "../../Forms/calculator";
+import styles from "../../../styles/HeroHome.module.scss";
 
 const HeroHome = () => {
   return (
     <section className={styles.heroContainer}>
       <div className="container">
-        <div>
-          <h1 className={styles.title}>
-            Calculate <span className={styles.lineDeco}>your CPM</span>{" "}
-            <span className={styles.lineBreak}></span> Stay on{" "}
-            <span className={styles.lineDeco}> top of monetization.</span>
-          </h1>
-          <CalculatorForm />
-        </div>
+        <div className={styles.group}>
+          <div>
+            <h1 className={styles.title}>
+              Calculate <span className={styles.lineDeco}>your CPM</span>{" "}
+              <span className={styles.lineBreak}></span> Stay on{" "}
+              <span className={styles.lineDeco}> top of monetization.</span>
+            </h1>
+            <CalculatorForm />
+          </div>
 
-        <div></div>
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://res.cloudinary.com/dqove2qhg/image/upload/v1656426401/CPM%20Calculators/hero-image_ndouct.svg"
+              alt="Vercel Logo"
+              width={400}
+              height={400}
+              layout="responsive"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
