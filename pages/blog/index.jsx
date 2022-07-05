7eec1fb273bcede2794d12cfdd782721452add01
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-07-05 10:23:23 +0530

    addition of post content on blog homepage

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,7 +1,12 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 import Seo from "../../components/Seo";
 import Layout from "../../components/Layout";
+import sortBydate from "../../utilis/sortBydate";
 
-const ImproveRevenue = () => {
+const ImproveRevenue = ({ posts }) => {
+  console.log("posts", posts);
   return (
     <>
       <Seo title="Improve Revenue | CPM Calculators" description="" />
@@ -11,4 +16,33 @@ const ImproveRevenue = () => {
   );
 };
 
+export async function getStaticProps() {
+  // Get files from the posts dir
+  const files = fs.readdirSync(path.join("posts"));
+
+  // Get slug and frontmatter from posts
+  const posts = files.map((filename) => {
+    // Create slug
+    const slug = filename.replace(".md", "");
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts: posts.sort(sortBydate),
+    },
+  };
+}
 export default ImproveRevenue;
---
+++ posts/what-is-ad-fill-rate.md
@@ -0,0 +1,13 @@
+---
+title: "What is Ad Fill Rate?"
+date: "July 5, 2022"
+excerpt: "Introduction Modern-day websites are attractive digital constructs that relay pertinent information to the visitors while maximizing their user experience. The primary objective…"
+# cover_image: '/images/posts/img3.jpg'
+---
+
+Lorem markdownum fine incustoditam unda factura versum occuluere Aeneas, iuvat
+haec praepes [partes epulae](http://cui.com/), in egisse de. Caecisque ter
+manus. Munere in exhalat, ferre sed [habe quaeque saepe](http://ne.org/fretum)
+verba caput ferarum _nubila_? Patriam Cyparisse tamen, **saxum** fide postponere
+pavida ne omnes etiam, atque. Sonuit omina sed sine haerebat illic fit a mora
+in.
