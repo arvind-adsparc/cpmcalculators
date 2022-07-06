28ef5e59ab9b767565725400af897f09e4109884
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-07-06 12:47:56 +0530

    addition of metrics page content

---
+++ metrics/cost-per-click.md
@@ -0,0 +1,16 @@
+---
+id: "1"
+title: "Cost Per Click"
+tag: "metrics"
+date: "September 23, 2021"
+excerpt: "The Page RPM metric supports publishers to recognize the actual performance of their ads on a page level. This way, a publisher can identify low-earning pages and..."
+src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1657016363/CPM%20Calculators/RPM_uuu8hm.jpg"
+cover_image: "https://res.cloudinary.com/dqove2qhg/image/upload/v1657016363/CPM%20Calculators/RPM_uuu8hm.jpg"
+---
+
+Lorem markdownum fine incustoditam unda factura versum occuluere Aeneas, iuvat
+haec praepes [partes epulae](http://cui.com/), in egisse de. Caecisque ter
+manus. Munere in exhalat, ferre sed [habe quaeque saepe](http://ne.org/fretum)
+verba caput ferarum _nubila_? Patriam Cyparisse tamen, **saxum** fide postponere
+pavida ne omnes etiam, atque. Sonuit omina sed sine haerebat illic fit a mora
+in.
---
+++ pages/ad-metrics/[slug].js
@@ -0,0 +1,70 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { marked } from "marked";
+import Image from "next/image";
+import Layout from "../../components/Layout";
+import styles from "../../styles/BlogPage.module.scss";
+import Newsletter from "../../components/Newsletter";
+
+const MetricsPage = ({
+  frontmatter: { title, date, cover_image },
+  slug,
+  content,
+}) => {
+  return (
+    <Layout>
+      <div className={styles.blogContainer}>
+        <div className="container">
+          <h1 className={styles.title}>{title}</h1>
+
+          <div className={styles.imageDiv}>
+            <Image src={cover_image} alt={title} width={900} height={500} />
+          </div>
+
+          <div className={styles.group}>
+            <div className={styles.content}>
+              <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+            </div>
+            <div className="sidebar"></div>
+          </div>
+        </div>
+      </div>
+      <Newsletter />
+    </Layout>
+  );
+};
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("metrics"));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const markdownWithMeta = fs.readFileSync(
+    path.join("metrics", slug + ".md"),
+    "utf-8"
+  );
+
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      frontmatter,
+      slug,
+      content,
+    },
+  };
+}
+
+export default MetricsPage;
