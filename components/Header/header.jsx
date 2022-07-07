6b21f34e789efefc0d7241fe9e4fd4f240cae821
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-07-07 18:23:02 +0530

    changes

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Dropdown } from "antd";
+import { Collapse } from "antd";
 import MenuOne from "./Menu/menuOne";
 import MenuTwo from "./Menu/menuTwo";
 import styles from "../../styles/Header.module.scss";
+import { useState } from "react";
+
+const { Panel } = Collapse;
 
 const Header = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+  const onNavChange = (key) => {
+    console.log(key);
+  };
   return (
     <header className={styles.header}>
       <div className="container groupTwo">
@@ -17,7 +25,7 @@ const Header = () => {
           </Link>
         </div>
         <nav className={styles.navMob}>
-          <div className="nav-icon">
+          <div onClick={() => setToggleNav(!toggleNav)} className="nav-icon">
             <Image
               src="https://res.cloudinary.com/dqove2qhg/image/upload/v1656065276/CPM%20Calculators/nav_icon_pl11bj.svg"
               alt="Vercel Logo"
@@ -29,6 +37,24 @@ const Header = () => {
             {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
           </div>
         </nav>
+
+        {toggleNav && (
+          <div className={styles.mobileCollapse}>
+            <Collapse onChange={onNavChange} expandIconPosition="end">
+              <Panel header="Calculators" key="1">
+                <p>Link</p>
+              </Panel>
+            </Collapse>
+
+            <Collapse onChange={onNavChange} expandIconPosition="end">
+              <Panel header="Ad Metrics" key="2">
+                <p>Link</p>
+              </Panel>
+            </Collapse>
+
+            <div className={styles.link}>Improve Revenue</div>
+          </div>
+        )}
 
         <nav className={styles.navDesk}>
           <div className={styles.links}>
