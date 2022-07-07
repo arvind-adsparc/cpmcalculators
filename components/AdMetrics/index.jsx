d108b59bfc115eefd7c24a9e7815807ee3528c3e
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-07-07 16:17:01 +0530

    addition of ad metrics links

--- components/AdMetrics/index.jsx
+++ components/AdMetrics/index.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../../styles/AdMetrics.module.scss";
 
 const data = [
@@ -6,36 +7,43 @@ const data = [
     id: "Cost Per Click (CPC)",
     title: "Cost Per Click (CPC)",
     src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656073535/CPM%20Calculators/cpc_qmo9mc.svg",
+    link: "/ad-metrics/cost-per-click",
   },
   {
     id: "EPMV",
     title: "EPMV",
     src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656073535/CPM%20Calculators/epmv_zk5b7c.svg",
+    link: "/ad-metrics/epmv",
   },
   {
     id: "eCPM",
     title: "eCPM",
     src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656073535/CPM%20Calculators/eCPM_ryd3ey.svg",
+    link: "/ad-metrics/ecpm",
   },
   {
     id: "Page RPM",
     title: "Page RPM",
     src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656073535/CPM%20Calculators/rpm_tararq.svg",
+    link: "/ad-metrics/page-rpm",
   },
   {
     id: "Cost Per Mile (CPM)",
     title: "Cost Per Mile (CPM)",
     src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656073535/CPM%20Calculators/cpm_b4oami.svg",
+    link: "/ad-metrics/cost-per-mile",
   },
   {
     id: "Ad  Viewability",
     title: "Ad  Viewability",
     src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656073542/CPM%20Calculators/ad_viewability_vamhbc.svg",
+    link: "/ad-metrics/ad-viewability",
   },
   {
     id: "Ad  Impressions",
     title: "Ad  Impressions",
     src: "https://res.cloudinary.com/dqove2qhg/image/upload/v1656073542/CPM%20Calculators/ad_impressions_ld5vyt.svg",
+    link: "/ad-metrics/ad-impressions",
   },
 ];
 
@@ -49,17 +57,21 @@ const AdMetrics = () => {
           {data.map((info) => {
             return (
               <div key={info.id} className={styles.metric}>
-                <div className={styles.imageDiv}>
-                  <Image
-                    src={info.src}
-                    alt="Vercel Logo"
-                    width={100}
-                    height={100}
-                    layout="responsive"
-                  />
-                </div>
+                <Link href={info.link}>
+                  <a>
+                    <div className={styles.imageDiv}>
+                      <Image
+                        src={info.src}
+                        alt="Vercel Logo"
+                        width={100}
+                        height={100}
+                        layout="responsive"
+                      />
+                    </div>
 
-                <div className={styles.text}>{info.title}</div>
+                    <div className={styles.text}>{info.title}</div>
+                  </a>
+                </Link>
               </div>
             );
           })}
