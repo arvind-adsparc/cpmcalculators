3193d07ebd502dc1c786fe54e8e677eca1c540e7
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-07-08 10:58:18 +0530

    addition of cpm form schema

---
+++ components/Forms/schema.js
@@ -0,0 +1,23 @@
+export const cpmFormSchema = [
+  {
+    name: "impressions",
+    label: "Impressions",
+    type: "number",
+    placeholder: "This is how many times the ad is viewed on a website",
+    value: "",
+  },
+  {
+    name: "cpm",
+    label: "CPM($)",
+    type: "number",
+    placeholder: "Cost per thousand impressions in one ad unit",
+    value: "",
+  },
+  {
+    name: "campaignCost",
+    label: "Campaign Cost($",
+    type: "number",
+    placeholder: "Total budget for the campaign",
+    value: "",
+  },
+];
