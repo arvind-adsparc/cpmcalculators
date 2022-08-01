e3de6c1319c9950f6ee0e3eb10e8b95ea72708cc
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-08-01 13:26:59 +0530

    google analytics changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,10 @@
 import "antd/dist/antd.css";
-import Script from "next/script";
+// import Script from "next/script";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        // strategy="lazyOnload"
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.GTAG_ID}', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script>
       <Component {...pageProps} />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-236091895-1"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-236091895-1', { page_path: window.location.pathname });
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
